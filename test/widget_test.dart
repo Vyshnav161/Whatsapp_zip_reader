c3e8d7540cf24f03048caa47b7ccb682f9ebc835
Vyshnav161 <vychumachu@gmail.com>
2025-07-30 14:11:32 +0530

    Name Chage

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:whatsappzip/main.dart';
+import 'package:WZipChat/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
